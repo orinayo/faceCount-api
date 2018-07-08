938065a2359353f8484b0260cd4070a323aa4832
orinayo <orinayooyelade@gmail.com>
2018-07-08 03:10:08 +0100

    uploaded api

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const bcrypt = require('bcrypt-nodejs')
+const knex = require('knex')
+
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'letmepass',
+    database: 'smart-brain'
+  }
+})
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send({
+    user: 'segun'
+  })
+})
+
+app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt))
+app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt))
+app.get('/profile/:id', (req, res) => profile.getUserProfile(req, res, db))
+app.put('/image', (req, res) => image.handleNewEntry(req, res, db))
+app.post('/imageurl', (req, res) => image.handleApiCall(req, res))
+
+app.listen(3001)
